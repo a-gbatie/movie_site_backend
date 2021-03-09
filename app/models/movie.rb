d027bc66131e156e87d93bc8ad024b28c96a0240
a-gbatie <ange.batie@gmail.com>
2021-03-08 16:24:40 -0800

    models, relationships, & seeds created

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,4 @@
 class Movie < ApplicationRecord
     has_many :comments
-    has_many :users, through: :comments
+    has_many :users, through: :comments, :dependent => :delete_all
 end
--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,3 +1,4 @@
 class MovieSerializer < ActiveModel::Serializer
   attributes :id, :name, :release_date, :image
+  has_many :users
 end
