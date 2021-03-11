9c4fa5d222cf4c8292256fe8d4c0e67db647de9d
a-gbatie <ange.batie@gmail.com>
2021-03-10 19:30:13 -0800

    3/10/2021 changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_194804) do
+ActiveRecord::Schema.define(version: 2021_03_10_191418) do
 
   create_table "comments", force: :cascade do |t|
     t.string "comments"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_03_08_194804) do
     t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "likes"
   end
 
   create_table "users", force: :cascade do |t|
